8b22f5bb75388faf78ccd46ba951d4b99abd0ddc
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2024-12-06 17:45:21 +0100

    walking function

--- day06/guard.py
+++ day06/guard.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from dataclasses import dataclass
 from enum import Enum
-
+from copy import deepcopy
 
 class Directions(Enum):
     UP = "^"
@@ -44,8 +44,35 @@ class Cell():
     visited: bool
     direction: list
 
+
+def walk(labmap: list, guard: Guard):
+    g = Guard(0,0,Directions.NONE)
+    g.x = guard.x
+    g.y = guard.y
+    g.direction = guard.direction
+    lab = deepcopy(labmap)
+    xlimit = len(lab[0])
+    ylimit = len(lab)
+    while g.y >= 0 and g.y < ylimit and g.x >= 0 and g.x < xlimit:
+        dx, dy = g.step()
+        if g.y + dy >= 0 and g.y + dy < ylimit and g.x + dx >= 0 and g.x + dx < xlimit:
+            lookup_cell = lab[g.y + dy][g.x + dx]
+            if not lookup_cell.wall:
+                g.y += dy
+                g.x += dx
+                lab[g.y][g.x].visited=True
+                lab[g.y][g.x].direction.append(g.direction)
+            else:
+                g.turn()
+        else:
+            break
+    tot = 0
+    for line in lab:
+        tot += len([cell for cell in line if cell.visited==True])
+    return tot
+
 def main():
-    with open("input", "r") as reader:
+    with open("example", "r") as reader:
         line = reader.readline()
         y = 0
         labmap = []
@@ -62,25 +89,7 @@ def main():
                 x += 1
             line = reader.readline()
             y += 1
-    xlimit = len(labmap[0])
-    ylimit = len(labmap)
-    while guard.y >= 0 and guard.y < ylimit and guard.x >= 0 and guard.x < xlimit:
-        dx, dy = guard.step()
-        if guard.y + dy >= 0 and guard.y + dy < ylimit and guard.x + dx >= 0 and guard.x + dx < xlimit:
-            lookup_cell = labmap[guard.y + dy][guard.x + dx]
-            if not lookup_cell.wall:
-                guard.y += dy
-                guard.x += dx
-                labmap[guard.y][guard.x].visited=True
-                labmap[guard.y][guard.x].direction.append(guard.direction)
-            else:
-                guard.turn()
-        else:
-            break
-    tot = 0
-    for line in labmap:
-        tot += len([cell for cell in line if cell.visited==True])
-    print(tot)
+    print(walk(labmap,guard))
 
 
 if __name__ == "__main__":
