12623ea363f3c594927d003f597508260b7e3800
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2024-12-10 10:36:07 +0100

    p2

--- day07/operators.py
+++ day07/operators.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 import sys
 
-DEBUG = True
+DEBUG = False
+
 
 def base3(i: int) -> str:
-    r = str(i%3)
-    while i:=i//3:
-        r = r+str(i%3)
+    r = str(i % 3)
+    while i := i // 3:
+        r = r + str(i % 3)
     return r[::-1]
+
 
 def oplist(n: int):
     return [base3(op).zfill(n) for op in range(3**n)]
 
 
 def main():
-    calibration = 0
+    calibration = [0, 0]
     while line := sys.stdin.readline():
         line = line.rstrip()
         test_result = int(line.split(":")[0])
@@ -24,8 +26,6 @@ def main():
         num_op = len(input_numbers) - 1
         operators = oplist(num_op)
         for operations in operators:
-            if DEBUG:
-                print(operations)
             acc = input_numbers[0]
             DEBUG_STRING = str(acc)
             for pos, op in enumerate(operations):
@@ -35,14 +35,20 @@ def main():
                 elif op == "1":
                     DEBUG_STRING = DEBUG_STRING + f" + {input_numbers[pos+1]}"
                     acc += input_numbers[pos + 1]
+                elif op == "2":
+                    DEBUG_STRING = DEBUG_STRING + f" || {input_numbers[pos+1]}"
+                    acc = int(f"{acc}{input_numbers[pos+1]}")
                 if acc > test_result:
                     break
             if test_result == acc:
                 if DEBUG:
                     print(f"{DEBUG_STRING} = {acc}")
-                calibration += test_result
+                if "||" in DEBUG_STRING:
+                    calibration[1] += test_result
+                else:
+                    calibration[0] += test_result
                 break
-    print(calibration)
+    print(calibration[0], sum(calibration))
 
 
 if __name__ == "__main__":
