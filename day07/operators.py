feb89bac9ea46eb8fd8df9c585905ab655e3d69c
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2024-12-10 10:08:01 +0100

    d1 rework

--- day07/operators.py
+++ day07/operators.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python3
 import sys
 
+DEBUG = False
+
+
 def oplist(n: int):
-    return [ format(op,'b').zfill(n).replace('0','*').replace('1','+') for op in range(2**n) ]
+    return [format(op, "b").zfill(n) for op in range(2**n)]
+
 
 def main():
     calibration = 0
     while line := sys.stdin.readline():
         line = line.rstrip()
-        test_result = int(line.split(':')[0])
-        input_numbers = [ int(n) for n in line.split(':')[1].split(' ')[1:] ]
-        #print(test_result,input_numbers)
-        num_op = len(input_numbers)-1
+        test_result = int(line.split(":")[0])
+        input_numbers = [int(n) for n in line.split(":")[1].split(" ")[1:]]
+        if DEBUG:
+            print("test:{} values:{}".format(test_result, input_numbers))
+        num_op = len(input_numbers) - 1
         operators = oplist(num_op)
-        print(operators)
-        concat_operations = []
-        for opnum,operations in enumerate(operators):
-            concat_operations.append([])
-            for pos,op in enumerate(operations):
-                concat_operations[opnum].append(f"{input_numbers[pos]} {op} ")
-            concat_operations[opnum].append(f"{input_numbers[-1]}")
-        possible_results = []
-        for q in concat_operations:
-            query_string="".join(q)
-            query = query_string.split(" ")
-            acc = int(query[0])
-            for k in range(len(query[1:])//2):
-                evaluate="{accumulator} {operation} {value}".format(accumulator=str(acc),operation=query[2*k+1],value=query[2*k+2])
-                acc = eval(evaluate)
-            possible_results.append(acc)
-        if test_result in possible_results:
-            calibration+=test_result
+        for operations in operators:
+            acc = input_numbers[0]
+            DEBUG_STRING = str(acc)
+            for pos, op in enumerate(operations):
+                if op == "0":
+                    DEBUG_STRING = DEBUG_STRING + f" * {input_numbers[pos+1]}"
+                    acc *= input_numbers[pos + 1]
+                elif op == "1":
+                    DEBUG_STRING = DEBUG_STRING + f" + {input_numbers[pos+1]}"
+                    acc += input_numbers[pos + 1]
+                if acc > test_result:
+                    break
+            if test_result == acc:
+                if DEBUG:
+                    print(f"{DEBUG_STRING} = {acc}")
+                calibration += test_result
+                break
     print(calibration)
-
 
 
 if __name__ == "__main__":
