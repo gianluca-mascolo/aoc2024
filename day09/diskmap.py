0b7cb9aa15864aba28cba4bc116fad1dc349e22a
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2024-12-11 11:17:41 +0100

    defrag

--- day09/diskmap.py
+++ day09/diskmap.py
@@ -2,6 +2,8 @@
 import sys
 from dataclasses import dataclass
 
+DEBUG = True
+
 
 @dataclass
 class File:
@@ -20,26 +22,50 @@ class Disk:
                 self.fat.extend([None] * repeat)
             else:
                 self.fat.extend([File(id=position // 2, blocks=repeat)] * repeat)
-    
-    def compacted(self):
+
+    @property
+    def compacted(self) -> bool:
         if None in self.fat:
             empty = self.fat.index(None)
-            return all(b == None for b in self.fat[empty:])
+            return all(b is None for b in self.fat[empty:])
         else:
             return True
+
+    @property
+    def size(self) -> int:
+        return len(self.fat)
+
+    @property
+    def checksum(self) -> int:
+        return sum(b * self.fat[b].id for b in range(self.size) if self.fat[b])
 
     def __str__(self):
         return "".join([str(b.id) if b else "." for b in self.fat])
 
 
+def defrag(disk: Disk):
+    while not disk.compacted:
+        size = disk.size
+        for b in range(size):
+            if disk.fat[b] is None:
+                empty = b
+                break
+        for b in range(size - 1, 0, -1):
+            if disk.fat[b] is not None:
+                full = b
+                break
+        disk.fat[empty], disk.fat[full] = disk.fat[full], disk.fat[empty]
+        if DEBUG:
+            print(disk)
+
+
 def main():
     while line := sys.stdin.readline():
         disk = Disk(line.rstrip())
-    # print(disk)
-    # for b in disk.fat:
-    #     print(b)
-    print(disk)
-    print(disk.compacted())
+    if DEBUG:
+        print(disk)
+    defrag(disk)
+    print(disk.checksum)
 
 
 if __name__ == "__main__":
