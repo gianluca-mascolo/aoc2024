6cc092bb90c2670e33303b46c90da60148f712bb
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2024-12-11 15:38:52 +0100

    d11.p1

---
+++ day11/stones.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+
+DEBUG = False
+
+def blink(stones: list):
+    new_stones = []
+    for s in stones:
+        if s == 0:
+            new_stones.append(1)
+        elif len(str(s)) % 2 == 0:
+            split = len(str(s)) // 2
+            new_stones.append(int(str(s)[:split]))
+            new_stones.append(int(str(s)[split:]))
+        else:
+            new_stones.append(s*2024)
+    return new_stones
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--blink", type=int, help="number of blinks", default=25)
+    args = parser.parse_args()
+    while line := sys.stdin.readline():
+        stones = [ int(c) for c in line.rstrip().split(" ") ]
+    if DEBUG:
+        print(stones)
+    for cycle in range(args.blink):
+        stones = blink(stones)
+        if DEBUG:
+            print(stones)
+        print(cycle,len(stones))
+
+
+if __name__ == "__main__":
+    main()
