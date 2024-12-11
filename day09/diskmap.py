b97548179293a105e75f5d7b6e1189557aa10723
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2024-12-11 06:58:01 +0100

    implent file class

--- day09/diskmap.py
+++ day09/diskmap.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
+from dataclasses import dataclass
 import sys
 
+@dataclass
+class File:
+    id: int
+    blocks: int
 
 class Disk:
     def __init__(self, dm: str):
         self.map = dm
+        self.fat = [ File(id=b//2,blocks=dm[b]) for b in range(0,len(dm),2) ]
+        #self.blocks = [ for b in dm ]
 
     def __repr__(self):
         class_name = type(self).__name__
@@ -25,6 +32,7 @@ def main():
     while line := sys.stdin.readline():
         disk = Disk(line.rstrip())
     print(disk)
+    print(disk.fat)
 
 
 if __name__ == "__main__":
