c39291f71343c20b666d0ac7211c26578f91947e
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2024-12-17 10:09:33 +0100

    fmt

--- day12/fences.py
+++ day12/fences.py
@@ -1,26 +1,37 @@
 #!/usr/bin/env python3
+import argparse
 from dataclasses import dataclass
 
-@dataclass(eq=True,frozen=True)
+DEBUG = False
+
+
+@dataclass(eq=True, frozen=True)
 class Fence:
     x: int
     y: int
     kind: bytes
 
+
 def nearby(fence: Fence, region: set) -> bool:
-    near = False
     for cell in region:
-        xdist = abs(cell.x-fence.x)
-        ydist = abs(cell.y-fence.y)
-        print(f"distance {xdist} {ydist}")
+        xdist = abs(cell.x - fence.x)
+        ydist = abs(cell.y - fence.y)
+        if DEBUG:
+            print(f"distance {xdist} {ydist}")
         if (xdist + ydist) <= 1:
-            near = True
-            break
-    return near
+            return True
+    return False
 
 
 def main():
-    with open("example3", "r") as reader:
+    global DEBUG
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", help="Debug ouput", action="store_true")
+    parser.add_argument("filename")
+    args = parser.parse_args()
+    if args.debug:
+        DEBUG = True
+    with open(args.filename, "r") as reader:
         fencemap = []
         fencekind = set()
         y = 0
@@ -28,15 +39,15 @@ def main():
         while line != "":  # The EOF char is an empty string
             line = line.rstrip()
             xlimit = len(line)
-#            fencemap.append([c for c in line])
-            fencemap.extend([Fence (x,y,f) for x, f in enumerate(line)])
+            #            fencemap.append([c for c in line])
+            fencemap.extend([Fence(x, y, f) for x, f in enumerate(line)])
 
             fencekind |= {c for c in line}
             line = reader.readline()
             y += 1
     ylimit = y
-    #print(fencemap)
-    #print(list(filter(lambda x: x.kind == 'A', fencemap)))
+    # print(fencemap)
+    # print(list(filter(lambda x: x.kind == 'A', fencemap)))
     regions = dict()
     for k in fencekind:
         regions[k] = []
@@ -48,16 +59,15 @@ def main():
                 found = False
                 for region in regions[k]:
                     print(f"fence: {f} region: {region}")
-                    if nearby(f,region):
+                    if nearby(f, region):
                         print("Add to region")
                         found = True
                         region |= {f}
                 if not found:
                     print(f"new region (seen) {f.x} {f.y} {f.kind}")
                     regions[k].append({f})
-    for k,v in regions.items():
-        print(k,len(v))
-
+    for k, v in regions.items():
+        print(k, len(v))
 
 
 if __name__ == "__main__":
