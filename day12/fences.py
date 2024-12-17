c1400aa879b236d8e8935e587a54884b992e7fcb
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2024-12-17 16:48:19 +0100

    regions attempt

--- day12/fences.py
+++ day12/fences.py
@@ -24,30 +24,49 @@ def main():
     if args.debug:
         DEBUG = True
     with open(args.filename, "r") as reader:
-        # fencemap = []
         gardens = dict()
-        # fencekind = set()
         y = 0
         line = reader.readline()
         while line != "":  # The EOF char is an empty string
             line = line.rstrip()
-            #            fencemap.append([c for c in line])
             for x, f in enumerate(line):
                 gardens[(x, y)] = Plot(f)
                 if (x - 1, y) in gardens and gardens[(x - 1, y)].kind == f:
-                    print(f"{y} {f} left")
+                    if DEBUG:
+                        print(f"{y} {f} left")
                     gardens[(x - 1, y)].right = (x, y)
                     gardens[(x, y)].left = (x - 1, y)
                 if (x, y - 1) in gardens and gardens[(x, y - 1)].kind == f:
-                    print(f"{y} {f} up")
+                    if DEBUG:
+                        print(f"{y} {f} up")
                     gardens[(x, y - 1)].down = (x, y)
                     gardens[(x, y)].up = (x, y - 1)
-                # fencemap.extend([Fence(Cell(x, y), f)])
-
-            # fencekind |= {c for c in line}
             line = reader.readline()
             y += 1
-    print(gardens)
+    if DEBUG:
+        print(gardens)
+
+    regions = []
+    for p in gardens.keys():
+        print("check", p, gardens[p])
+        for direction in ["left", "right", "up", "down"]:
+            if connected := getattr(gardens[p], direction):
+                print("conn1", connected)
+                for r in regions:
+                    if connected in r:
+                        r |= {p}
+
+        if any(p in r for r in regions):
+            print("trovo")
+        else:
+            regions.append({p})
+            for direction in ["left", "right", "up", "down"]:
+                if connected := getattr(gardens[p], direction):
+                    print("conn", connected)
+                    regions[-1] |= {connected}
+    print("XXX")
+    for r in regions:
+        print(r)
 
 
 if __name__ == "__main__":
