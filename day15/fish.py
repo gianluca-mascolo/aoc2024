eb359a7c838774e3004e672680930b39905817e9
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2024-12-24 09:37:22 +0100

    rewrite from scratch

--- day15/fish.py
+++ day15/fish.py
@@ -6,91 +6,126 @@ DEBUG = False
 DIRECTIONS = {"<": (-1, 0), "^": (0, -1), "v": (0, 1), ">": (1, 0)}
 
 
-def move(cell: tuple, maze: list, direction: bytes):
-    xlimit = len(maze[0])
-    ylimit = len(maze)
-    dx = DIRECTIONS[direction][0]
-    dy = DIRECTIONS[direction][1]
-    x = cell[0]
-    y = cell[1]
-    if x + dx < 0 or x + dx >= xlimit or y + dy < 0 or y + dy >= ylimit:
-        return cell
-    elif maze[y + dy][x + dx] == "#":
-        return cell
-    elif maze[y + dy][x + dx] == ".":
-        if maze[y][x] == "[" and maze[y + dy][x + dx + 1] in [".", "["]:
-            maze[y][x] = '.'
-            maze[y][x+1] = '.'
-            maze[y + dy][x + dx] = '['
-            maze[y + dy][x + dx + 1] = ']'
-        elif maze[y][x] == "]" and maze[y + dy][x + dx - 1] in [".", "]"]:
-            maze[y][x] = '.'
-            maze[y][x-1] = '.'
-            maze[y + dy][x + dx] = ']'
-            maze[y + dy][x + dx - 1] = '['
+# def move(cell: tuple, maze: list, direction: bytes):
+#     xlimit = len(maze[0])
+#     ylimit = len(maze)
+#     dx = DIRECTIONS[direction][0]
+#     dy = DIRECTIONS[direction][1]
+#     x = cell[0]
+#     y = cell[1]
+#     if x + dx < 0 or x + dx >= xlimit or y + dy < 0 or y + dy >= ylimit:
+#         return cell
+#     elif maze[y + dy][x + dx] == "#":
+#         return cell
+#     elif maze[y + dy][x + dx] == ".":
+#         if maze[y][x] == "[" and maze[y + dy][x + dx + 1] in [".", "["]:
+#             maze[y][x] = '.'
+#             maze[y][x+1] = '.'
+#             maze[y + dy][x + dx] = '['
+#             maze[y + dy][x + dx + 1] = ']'
+#         elif maze[y][x] == "]" and maze[y + dy][x + dx - 1] in [".", "]"]:
+#             maze[y][x] = '.'
+#             maze[y][x-1] = '.'
+#             maze[y + dy][x + dx] = ']'
+#             maze[y + dy][x + dx - 1] = '['
+#         else:
+#             maze[y][x], maze[y + dy][x + dx] = maze[y + dy][x + dx], maze[y][x]
+#         return (x + dx, y + dy)
+#     elif maze[y + dy][x + dx] == "O":
+#         newpos = move(cell=(x + dx, y + dy), maze=maze, direction=direction)
+#         if newpos != (x + dx, y + dy):
+#             maze[y][x], maze[y + dy][x + dx] = maze[y + dy][x + dx], maze[y][x]
+#             return (x + dx, y + dy)
+#         else:
+#             return cell
+#     elif maze[y + dy][x + dx] == "[" or maze[y + dy][x + dx] == "]":
+#         print(f"JUMP {maze[y + dy][x + dx]} {x} {y} {dx} {dy}")
+#         newpos = move(cell=(x + 2 * dx, y + dy), maze=maze, direction=direction)
+#         print(f"NP: {newpos} cell: {cell} delta: {DIRECTIONS[direction]}")
+#         xshift = x+2*dx-newpos[0]
+#         yshift = y+2*dy-newpos[1]
+#         if xshift or yshift:
+#             print(f"SHIFT: {xshift} {yshift}")
+#             if maze[y+dy][x+dx] == '[':
+#                 current = maze[y][x]
+#                 print(f"A {current}")
+#             elif maze[y+dy][x+dx] == ']':
+#                 current = maze[y][x]
+#                 print(f"B {current}")
+
+#             elif maze[y+dy][x+dx] == '.':
+#                 print("NORMAL")
+#                 if maze[y][x] == "[":
+#                     maze[y][x] = '.'
+#                     maze[y][x+1] = '.'
+#                     maze[y + dy][x + dx] = '['
+#                     maze[y + dy][x + dx + 1] = ']'
+#                 elif maze[y][x] == "[":
+#                     maze[y][x] = '.'
+#                     maze[y][x-1] = '.'
+#                     maze[y + dy][x + dx] = ']'
+#                     maze[y + dy][x + dx - 1] = '['
+#                 else:
+#                     maze[y][x], maze[y + dy][x + dx] = maze[y + dy][x + dx], maze[y][x]
+
+#             return(x+ dx, y+dy)
+#         else:
+#             return cell
+#     else:
+#         print("unknown")
+#         assert False
+
+
+# def inflate(maze: list):
+#     newmaze = []
+#     for line in maze:
+#         newline = []
+#         for cell in line:
+#             if cell == "#":
+#                 newline.extend(["#", "#"])
+#             elif cell == "O":
+#                 newline.extend(["[", "]"])
+#             elif cell == ".":
+#                 newline.extend([".", "."])
+#             elif cell == "@":
+#                 newline.extend(["@", "."])
+#         newmaze.append(newline)
+#     return newmaze
+
+
+class Maze:
+    def __init__(self):
+        self.map = {}
+        self.xlimit = 0
+        self.ylimit = 0
+
+    def put(self, coord: tuple, content: bytes):
+        self.map[coord] = content
+
+    def get(self, coord: tuple):
+        if coord in self.map:
+            return self.map[coord]
         else:
-            maze[y][x], maze[y + dy][x + dx] = maze[y + dy][x + dx], maze[y][x]
-        return (x + dx, y + dy)
-    elif maze[y + dy][x + dx] == "O":
-        newpos = move(cell=(x + dx, y + dy), maze=maze, direction=direction)
-        if newpos != (x + dx, y + dy):
-            maze[y][x], maze[y + dy][x + dx] = maze[y + dy][x + dx], maze[y][x]
-            return (x + dx, y + dy)
-        else:
-            return cell
-    elif maze[y + dy][x + dx] == "[" or maze[y + dy][x + dx] == "]":
-        print(f"JUMP {maze[y + dy][x + dx]} {x} {y} {dx} {dy}")
-        newpos = move(cell=(x + 2 * dx, y + dy), maze=maze, direction=direction)
-        print(f"NP: {newpos} cell: {cell} delta: {DIRECTIONS[direction]}")
-        xshift = x+2*dx-newpos[0]
-        yshift = y+2*dy-newpos[1]
-        if xshift or yshift:
-            print(f"SHIFT: {xshift} {yshift}")
-            if maze[y+dy][x+dx] == '[':
-                current = maze[y][x]
-                print(f"A {current}")
-            elif maze[y+dy][x+dx] == ']':
-                current = maze[y][x]
-                print(f"B {current}")
+            return None
 
-            elif maze[y+dy][x+dx] == '.':
-                print("NORMAL")
-                if maze[y][x] == "[":
-                    maze[y][x] = '.'
-                    maze[y][x+1] = '.'
-                    maze[y + dy][x + dx] = '['
-                    maze[y + dy][x + dx + 1] = ']'
-                elif maze[y][x] == "[":
-                    maze[y][x] = '.'
-                    maze[y][x-1] = '.'
-                    maze[y + dy][x + dx] = ']'
-                    maze[y + dy][x + dx - 1] = '['
-                else:
-                    maze[y][x], maze[y + dy][x + dx] = maze[y + dy][x + dx], maze[y][x]
+    def print(self):
+        for y in range(self.ylimit):
+            line = []
+            for x in range(self.xlimit):
+                line.append(self.get((x, y)))
+            print("".join(line))
 
-            return(x+ dx, y+dy)
-        else:
-            return cell
-    else:
-        print("unknown")
-        assert False
+    @property
+    def robot(self):
+        rpos = None
+        for coord, cell in self.map.items():
+            if cell == "@":
+                rpos = coord
+        return rpos
 
 
-def inflate(maze: list):
-    newmaze = []
-    for line in maze:
-        newline = []
-        for cell in line:
-            if cell == "#":
-                newline.extend(["#", "#"])
-            elif cell == "O":
-                newline.extend(["[", "]"])
-            elif cell == ".":
-                newline.extend([".", "."])
-            elif cell == "@":
-                newline.extend(["@", "."])
-        newmaze.append(newline)
-    return newmaze
+def move(maze: Maze, coord: tuple, direction: bytes):
+    return True
 
 
 def main():
@@ -106,7 +141,7 @@ def main():
     with open(args.filename, "r") as reader:
         line = reader.readline()
         loading = "MAZE"
-        maze = []
+        maze = Maze()
         moves = []
         y = 0
         while line != "":  # The EOF char is an empty string
@@ -116,50 +151,50 @@ def main():
                 line = reader.readline()
                 continue
             if loading == "MAZE":
-                ml = [c for c in line]
-                if "@" in ml:
-                    robot = (ml.index("@"), y)
-                maze.append(ml)
+                maze.xlimit = len(line)
+                for x, cell in enumerate(line):
+                    maze.put((x, y), cell)
+                y += 1
             elif loading == "MOVES":
                 moves.extend(m for m in line)
             line = reader.readline()
-            y += 1
+        maze.ylimit = y
         if DEBUG:
-            for line in maze:
-                print("".join(line))
+            maze.print()
             print(moves)
-            print(robot)
+            print(maze.robot)
+            print(maze.xlimit, maze.ylimit)
 
-        if args.part == 1:
-            for idx, direction in enumerate(moves):
-                if DEBUG:
-                    print(idx, robot, direction)
-                robot = move(cell=robot, maze=maze, direction=direction)
-                if DEBUG:
-                    for line in maze:
-                        print("".join(line))
-            result = 0
-            for y, line in enumerate(maze):
-                for x, cell in enumerate(line):
-                    if cell == "O":
-                        result += x + 100 * y
-            print(result)
-        else:
-            maze = inflate(maze)
-            for y, line in enumerate(maze):
-                for x, cell in enumerate(line):
-                    if cell == "@":
-                        robot = (x, y)
-            if DEBUG:
-                for line in maze:
-                    print("".join(line))
-            for idx, direction in enumerate(moves):
-                if DEBUG:
-                    print(idx, robot, direction)
-                robot = move(cell=robot, maze=maze, direction=direction)
-                if DEBUG:
-                    for line in maze:
-                        print("".join(line))
+        # if args.part == 1:
+        #     for idx, direction in enumerate(moves):
+        #         if DEBUG:
+        #             print(idx, robot, direction)
+        #         robot = move(cell=robot, maze=maze, direction=direction)
+        #         if DEBUG:
+        #             for line in maze:
+        #                 print("".join(line))
+        #     result = 0
+        #     for y, line in enumerate(maze):
+        #         for x, cell in enumerate(line):
+        #             if cell == "O":
+        #                 result += x + 100 * y
+        #     print(result)
+        # else:
+        #     maze = inflate(maze)
+        #     for y, line in enumerate(maze):
+        #         for x, cell in enumerate(line):
+        #             if cell == "@":
+        #                 robot = (x, y)
+        #     if DEBUG:
+        #         for line in maze:
+        #             print("".join(line))
+        #     for idx, direction in enumerate(moves):
+        #         if DEBUG:
+        #             print(idx, robot, direction)
+        #         robot = move(cell=robot, maze=maze, direction=direction)
+        #         if DEBUG:
+        #             for line in maze:
+        #                 print("".join(line))
 
 
 if __name__ == "__main__":
